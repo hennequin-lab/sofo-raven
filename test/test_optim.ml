87c8f553b170622a229ce32ec9dd915647cea487
Guillaume Hennequin <gje.hennequin@gmail.com>
2026-09-16 09:34:55 +0100

    Fix copyright headers: The SOFO authors
    The headers were copy-pasted from Raven; sofo is its own project.

--- test/test_optim.ml
+++ test/test_optim.ml
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-  Copyright (c) 2026 The Raven authors. All rights reserved.
+  Copyright (c) 2026 The SOFO authors. All rights reserved.
   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
